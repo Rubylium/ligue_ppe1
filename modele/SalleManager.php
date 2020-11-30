b5007ba7aa8290005f4065a383c742266a09b371
Rubylium <alexisoko0@gmail.com>
2020-11-30 15:06:21 +0100

    Feat(model+vue): Affiche des salles

--- modele/SalleManager.php
+++ modele/SalleManager.php
@@ -5,33 +5,17 @@ require_once("modele/Salle.php");
 
 class SalleManager extends Manager
 {
-    // public function get($id) //instancie un objet bateau
-    // {
-    //     $id = (int) $id;
-    //     $q = $this->getPDO()->query('SELECT id, nom, photo FROM bateau WHERE id = ' . $id);
-    //     $donnees = $q->fetch(PDO::FETCH_ASSOC);
-    //     return new Bateau($donnees['id'], $donnees['nom'], $donnees['photo']);
-    // }
 
-    public function getList() //instancie une collection d'objets bateau
+    public function getList() //instancie une collection d'objets Salle
     {
 
-        $capacite = [];
-        $q = $this->getPDO()->query('SELECT id, nom, photo FROM bateau ORDER BY nom');
+        $q = $this->getPDO()->query('SELECT nSalle, nomSalle, nbPoste, indIP FROM salle');
 
-
+        $salles = [];
         while ($donnees = $q->fetch(PDO::FETCH_ASSOC)) {
-            $capacite = [];
-            $b = $this->getPDO()->query("SELECT lettreCategorie, capaciteMax FROM contenance_bateau WHERE idBateau = " . $donnees['id']);
 
-            while ($donnee = $b->fetch(PDO::FETCH_ASSOC)) {
-                $capacite[$donnee['lettreCategorie']] = [
-                    $donnee['capaciteMax'],
-                ];
-            }
-
-            $bateaux[$donnees['id']] = new Salle($donnees['id'], $donnees['nom'], $donnees['photo'], $capacite);
+            $salles[$donnees['nSalle']] = new Salle($donnees['nSalle'], $donnees['nomSalle'], $donnees['nbPoste'], $donnees['indIP']);
         }
-        return $bateaux;
+        return $salles;
     }
 }

df01e5ad4a92676350f6876a23ffc8306b97f825
Rubylium <alexisoko0@gmail.com>
2020-11-30 14:47:42 +0100

    Feat(salle): Ajouts de la fonciton salle

--- modele/Salle.php
+++ modele/Salle.php
@@ -2,16 +2,37 @@
 
 class Salle
 {
-    private $id;
+    private $nSalle;
+    private $nomSalle;
+    private $nbPoste;
+    private $indIP;
 
 
-    public function __construct($id, $nom, $photo, $capacite)
+    public function __construct($nSalle, $nomSalle, $nbPoste, $indIP)
     {
         $this->id = $id;
+        $this->nomSalle = $nomSalle;
+        $this->nbPoste = $nbPoste;
+        $this->indIP = $indIP;
     }
 
-    public function getId()
+    public function getnSalle()
     {
-        return $this->id;
+        return $this->nSalle;
+    }
+
+    public function getnomSalle()
+    {
+        return $this->nomSalle;
+    }
+
+    public function getnbPoste()
+    {
+        return $this->nbPoste;
+    }
+
+    public function getindIP()
+    {
+        return $this->indIP;
     }
 }

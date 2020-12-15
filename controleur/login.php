a97009909e586b06970eaaba53f00a66c97170f9
Rubylium <alexisoko0@gmail.com>
2020-12-15 09:43:04 +0100

    Update login.php

--- controleur/login.php
+++ controleur/login.php
@@ -4,19 +4,19 @@ if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
     $racine = "..";
 }
 
-require_once("$racine/modele/userManager.php");
+require_once("$racine/modele/usersManager.php");
 
 
 
 $userManager = new usersManager(); // Création d'un objet
 $exist = $userManager->DoesUserExist($_POST["inputEmail"], $_POST["inputPassword"]);
 
-
 include "$racine/vue/header.php";
 if ($exist) {
-    include "$racine/vue/vueAcceuil.php";
+
+    include "$racine/vue/vueAccueilLogin.php";
 } else {
     $titre = "Indentifiant incorrecte";
-    include "$racine/vue/vueAcceuil.php";
+    include "$racine/vue/vueAccueil.php";
 }
 include "$racine/vue/footer.php";

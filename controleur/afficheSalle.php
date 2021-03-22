6858d676d968076a6717026d2824da4a52b3beb5
Rubylium <19718604+Rubylium@users.noreply.github.com>
2021-03-22 17:11:59 +0100

    Update afficheSalle.php

--- controleur/afficheSalle.php
+++ controleur/afficheSalle.php
@@ -5,15 +5,17 @@ if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
 }
 
 require_once("$racine/modele/SalleManager.php");
+require_once("$racine/modele/PosteManager.php");
 require_once("$racine/modele/AreaManager.php");
 
 
 
 $salleManager = new SalleManager(); // Création d'un objet
+$posteManager = new PosteManager(); // Création d'un objet
 $areaManager = new AreaManager(); // Création d'un objet
 $salles = $salleManager->getList();
 $areas = $areaManager->getList();
-
+$poste = $posteManager->getList();
 $titre = "Affichage de toute les salles";
 // appel du script de vue qui permet de gerer l'affichage des donnees
 include "$racine/vue/header.php";

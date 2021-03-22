bd708ca032547f43477d18b7f5f6f06d9207ab8a
Rubylium <19718604+Rubylium@users.noreply.github.com>
2021-03-22 17:11:49 +0100

    Create Poste.php

---
+++ modele/Poste.php
@@ -0,0 +1,60 @@
+<?php
+
+class Poste
+{
+    private $nPoste;
+    private $nomPoste;
+    private $indIP;
+    private $ad;
+    private $typePoste;
+    private $nSalle;
+    private $nbLog;
+
+
+    public function __construct($nPoste, $nomPoste, $indIP, $ad, $typePoste, $nSalle, $nbLog)
+    {
+        $this->$nPoste = $nPoste;
+        $this->$nomPoste = $nomPoste;
+        $this->$indIP = $indIP;
+        $this->$ad = $ad;
+        $this->$typePoste = $typePoste;
+        $this->$nSalle = $nSalle;
+        $this->$nbLog = $nbLog;
+    }
+
+
+    public function GetnPoste()
+    {
+        return $this->nPoste;
+    }
+
+    public function GetnomPoste()
+    {
+        return $this->nomPoste;
+    }
+
+    public function GetindIP()
+    {
+        return $this->indIP;
+    }
+
+    public function Getad()
+    {
+        return $this->ad;
+    }
+
+    public function GettypePoste()
+    {
+        return $this->typePoste;
+    }
+
+    public function GetnSalle()
+    {
+        return $this->nSalle;
+    }
+
+    public function GetnbLog()
+    {
+        return $this->nbLog;
+    }
+}

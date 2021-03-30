38db67272269f9dfcb485fe4e7d803f89db05ecb
Rubylium <19718604+Rubylium@users.noreply.github.com>
2021-03-30 17:31:09 +0200

    Create modifierPoste.php

---
+++ controleur/modifierPoste.php
@@ -0,0 +1,17 @@
+<?php
+
+if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
+    $racine = "..";
+}
+
+require_once("$racine/modele/PosteManager.php");
+
+
+
+$posteManager = new PosteManager();
+$poste = $posteManager->getList();
+$titre = "Modifier un poste";
+// appel du script de vue qui permet de gerer l'affichage des donnees
+include "$racine/vue/header.php";
+include "$racine/vue/vueModifierPoste.php";
+include "$racine/vue/footer.php";

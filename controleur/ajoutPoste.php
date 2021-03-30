a04765858dcc9e75243891928f07c601ff291009
Rubylium <19718604+Rubylium@users.noreply.github.com>
2021-03-30 15:38:17 +0200

    Update ajoutPoste.php

--- controleur/ajoutPoste.php
+++ controleur/ajoutPoste.php
@@ -4,18 +4,14 @@ if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
     $racine = "..";
 }
 
-require_once("$racine/modele/SalleManager.php");
-require_once("$racine/modele/PosteManager.php");
-require_once("$racine/modele/AreaManager.php");
+require_once("$racine/modele/InfoManager.php");
 
 
 
-$salleManager = new SalleManager(); // Création d'un objet
-$posteManager = new PosteManager(); // Création d'un objet
-$areaManager = new AreaManager(); // Création d'un objet
-$salles = $salleManager->getList();
-$areas = $areaManager->getList();
-$poste = $posteManager->getList();
+$infoManager = new InfoManager();
+$types = $infoManager->GetAllType();
+$ips = $infoManager->GetAllIp();
+$salle = $infoManager->GetAllSalle();
 $titre = "Ajouter un poste";
 // appel du script de vue qui permet de gerer l'affichage des donnees
 include "$racine/vue/header.php";

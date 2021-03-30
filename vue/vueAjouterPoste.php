718a02900a0347d9ca35303f70a48553e4662df1
Rubylium <19718604+Rubylium@users.noreply.github.com>
2021-03-30 15:38:30 +0200

    Update vueAjouterPoste.php

--- vue/vueAjouterPoste.php
+++ vue/vueAjouterPoste.php
@@ -11,23 +11,46 @@ if ($_SESSION["connected"] != null || $_SESSION["connected"] == true) {
 ?>
 
 	<div id="logreg-forms">
-		<form action="?action=login" method="post" class="form-signin">
+		<form action="?action=ajoutValide" method="post" class="form-signin">
 			<h1 class="h3 mb-3 font-weight-normal" style="text-align: center">Ajout de poste</h1>
 
 			<input type="num" name="inputNum" class="form-control" placeholder="Numero de poste" required="" autofocus="">
 			<input type="nom" name="inputNom" class="form-control" placeholder="Nom de poste" required="">
 
 			<select type="indIP" name="inputindIP" class="form-control">
-				<option>IP 1</option>
+				<?php
+					foreach ($ips as $v) {
+					?>
+						<option><?= $v ?></option>
+					<?php
+					}
+				?>
 			</select>
 
 			<input type="ad" name="inputAd" class="form-control" placeholder="ad" required="">
 
 			<select type="type" name="inputType" class="form-control">
-				<option>Type 1</option>
+				<?php
+					foreach ($types as $v) {
+					?>
+						<option><?= $v ?></option>
+					<?php
+					}
+				?>
 			</select>
 
-			<input type="nSalle" name="inputnSalle" class="form-control" placeholder="Numero salle" required="">
+			<select type="nSalle" name="inputnSalle" class="form-control">
+				<?php
+					foreach ($salle as $v) {
+					?>
+						<option><?= $v ?></option>
+					<?php
+					}
+				?>
+			</select>
+
+			<!-- <input type="nSalle" name="inputnSalle" class="form-control" placeholder="Numero salle" required=""> -->
+			<input type="NbLog" name="inputNbLog" class="form-control" placeholder="Nombre log (0, 1, 3)" required="">
 
 			<button class="btn btn-success btn-block" type="submit"><i class="fas fa-sign-in-alt"></i>Ajouter</button>
 			<hr>
